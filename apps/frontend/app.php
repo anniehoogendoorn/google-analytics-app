5778db0644f838449ed2517e76fb3d9af857ea74
Annie <anniehoogendoorn@gmail.com>
2016-02-09 15:15:59 -0800

    Add twig to files.

--- apps/frontend/app.php
+++ apps/frontend/app.php
@@ -1,15 +1,20 @@
 <?php
-/**
- * Load Google API library, Our ETL Classes, and authorization credentials
- */
+
+    /**
+     * Sets timezone for Symfony
+     */
+
+    date_default_timezone_set('America/Los_Angeles');
+    /**
+     * Load PHP library, Our ETL Classes, and database login info
+     */
+
 
     require_once __DIR__."/../../vendor/autoload.php";
     require_once __DIR__."/../../src/ReturnedAnalytics.php";
     require_once __DIR__."/../../src/Sites.php";
     require_once __DIR__."/../../src/user_data.php";
 
-    $app = new Silex\Application();
-    $app["debug"] = true;
 
     /**
      * Connect to database
@@ -29,14 +34,26 @@
       exit;
     }
 
+    $app = new Silex\Application();
+    $app["debug"] = true;
+
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views'
+    'twig.path' => __DIR__.'/../../views'
     ));
 
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
+    //This takes the user to "/" page, in which we render our index.html.twig page. It also pushes our categories to be listed.
+    $app->get("/", function() use ($app){
+    $sites = Sites::getAll();
+    $properties = get_object_vars($sites);
+    return $app['twig']->render('index.html.twig', array('sites' => $sites->getAll(), 'properties' => $properties));
+    });
 
 
+
+    return $app;
 
 ?>
